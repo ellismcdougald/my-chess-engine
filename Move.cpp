530d851ec4144c07963509dae98e28281a309be1
Ellis McDougald <ecmcdougald@gmail.com>
2024-01-07 16:30:11 -0700

    Sets up empty Move class

---
+++ Move.cpp
@@ -0,0 +1,6 @@
+#ifndef MOVE_CPP // GUARD
+#define MOVE_CPP // GUARD
+
+#include"Move.hpp"
+
+#endif // END GUARD
---
+++ Move.hpp
@@ -0,0 +1,8 @@
+#ifndef MOVE_HPP // GUARD
+#define MOVE_HPP // GUARD
+
+class Move {
+
+};
+
+#endif // END GUARD
