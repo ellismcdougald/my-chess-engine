0421726a5f1501c70bab23d371637845407ee8f2
Ellis McDougald <ecmcdougald@gmail.com>
2024-01-09 15:28:52 -0700

    Creates print_bitboard function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 #include "Board.cpp"
 #include "MoveGenerator.cpp"
 
+void print_bitboard(bitboard bb) {
+  bitboard mask = (bitboard) 1 << 63;
+  for(int i = 0; i < 64; i++) {
+    std::cout << (mask & bb ? 1 : 0);
+    mask >>= 1;
+    if((i + 1) % 8 == 0) std::cout << "\n";
+  }
+}	
+
 int main() {
   return 0;
 }
