4423d5006d5f2154752bbe4921df0e24f8106227
Ellis McDougald <ecmcdougald@gmail.com>
2024-01-12 15:08:39 -0700

    Implements rest of getters and move equality function

--- src/Move.cpp
+++ src/Move.cpp
@@ -19,4 +19,24 @@ bitboard Move::get_to_position() {
   return to_position;
 }
 
+BoardConstants::PIECE Move::get_move_piece() {
+  return move_piece;
+}
+
+BoardConstants::PIECE Move::get_capture_piece() {
+  return capture_piece;
+}
+
+bool Move::is_castle() {
+  return castle;
+}
+
+bool Move::move_equals(Move &other_move) {
+  return from_position == other_move.get_from_position() &&
+    to_position == other_move.get_to_position() &&
+    move_piece == other_move.get_move_piece() &&
+    capture_piece == other_move.get_capture_piece() &&
+    castle == other_move.is_castle();
+}
+
 #endif // END GUARD
