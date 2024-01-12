35c77d335e761d7d0d3eaeefb276a2645a8d6a77
Ellis McDougald <ecmcdougald@gmail.com>
2024-01-11 20:12:48 -0700

    Sets up move class

--- src/Move.cpp
+++ src/Move.cpp
@@ -3,4 +3,20 @@
 
 #include"Move.hpp"
 
+Move::Move(bitboard from, bitboard to, BoardConstants::PIECE move_p, BoardConstants::PIECE capture_p, bool is_castle) {
+  from_position = from;
+  to_position = to;
+  move_piece = move_p;
+  capture_p = capture_piece;
+  castle = is_castle;
+}
+
+bitboard Move::get_from_position() {
+  return from_position;
+}
+
+bitboard Move::get_to_position() {
+  return to_position;
+}
+
 #endif // END GUARD
--- src/Move.hpp
+++ src/Move.hpp
@@ -1,8 +1,34 @@
 #ifndef MOVE_HPP // GUARD
 #define MOVE_HPP // GUARD
 
-class Move {
+#include <stdint.h>
 
+#include "BoardConstants.hpp"
+
+typedef uint64_t bitboard;
+
+class Move {
+public:
+  // Constructors:
+  Move(bitboard from, bitboard to, BoardConstants::PIECE move_p, BoardConstants::PIECE capture_p, bool is_castle);
+
+  // Getters:
+  /**
+   * Returns the bitboard representing the starting position of the moving piece.
+   */
+  bitboard get_from_position();
+
+  /**
+   * Returns the bitboard representing the end position of the moving piece.
+   */
+  bitboard get_to_position();
+
+private:
+  bitboard from_position;
+  bitboard to_position;
+  BoardConstants::PIECE move_piece;
+  BoardConstants::PIECE capture_piece;
+  bool castle;
 };
 
 #endif // END GUARD
