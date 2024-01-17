0a8d4dae5d9aa9d7e006371c690947e5fd6d1892
Ellis McDougald <ecmcdougald@gmail.com>
2024-01-16 18:49:03 -0700

    Tests en passant move generation and knight move generation

--- src/tests/move_generator_tests.cpp
+++ src/tests/move_generator_tests.cpp
@@ -22,7 +22,6 @@ TEST_CASE("generate_pawn_pseudo_legal_moves works properly", "[pawn pseudo legal
   Board board;
   MoveGenerator move_gen;
 
-  /*
   SECTION("single white pawn on otherwise white board") {
     board.set_piece_positions(BoardConstants::PAWN, BoardConstants::WHITE, 0x800);
 
@@ -35,8 +34,6 @@ TEST_CASE("generate_pawn_pseudo_legal_moves works properly", "[pawn pseudo legal
     REQUIRE(move_vector_contains(expected_move_two, pawn_pseudo_legal_moves) == true);
     REQUIRE(pawn_pseudo_legal_moves.size() == 2);
   }
-  */
-
   
   SECTION("single white pawn on d4 vs single black pawn on e5") {
     board.set_piece_positions(BoardConstants::PAWN, BoardConstants::WHITE, 0x10000000);
@@ -66,3 +63,63 @@ TEST_CASE("generate_pawn_pseudo_legal_moves works properly", "[pawn pseudo legal
     REQUIRE(pawn_pseudo_legal_moves_black.size() == 2);
   }
 }
+
+TEST_CASE("generate_pseudo_legal_en_passant_moves works properly", "[en passant]") {
+  Board board;
+  MoveGenerator move_gen;
+
+  SECTION("generates both en passant moves when moving pawn is flanked on both sides") {
+    board.set_piece_positions(BoardConstants::PAWN, BoardConstants::WHITE, 0x800);
+    board.set_piece_positions(BoardConstants::PAWN, BoardConstants::BLACK, 0x14000000);
+
+    Move pawn_push(0x800, 0x8000000, BoardConstants::PAWN, BoardConstants::NONE, false);
+    board.execute_move(pawn_push, BoardConstants::WHITE);
+
+    std::vector<Move> black_en_passant_pseudo_legal_moves = move_gen.generate_pseudo_legal_en_passant_moves(board, BoardConstants::BLACK);
+
+    Move expected_move_one(0x10000000, 0x80000, BoardConstants::PAWN, BoardConstants::PAWN, false);
+    Move expected_move_two(0x4000000, 0x80000, BoardConstants::PAWN, BoardConstants::PAWN, false);
+
+    REQUIRE(black_en_passant_pseudo_legal_moves.size() == 2);
+    REQUIRE(move_vector_contains(expected_move_one, black_en_passant_pseudo_legal_moves) == true);
+    REQUIRE(move_vector_contains(expected_move_two, black_en_passant_pseudo_legal_moves) == true);
+  }
+
+  SECTION("no en passant moves if last move isn't a double push") {
+    board.set_piece_positions(BoardConstants::PAWN, BoardConstants::WHITE, 0x8000000);
+    board.set_piece_positions(BoardConstants::PAWN, BoardConstants::WHITE, 0x10000000);
+
+    std::vector<Move> black_en_passant_pseudo_legal_moves = move_gen.generate_pseudo_legal_en_passant_moves(board, BoardConstants::BLACK);
+    REQUIRE(black_en_passant_pseudo_legal_moves.size() == 0);
+  }
+}
+
+/*
+TEST_CASE("generate_knight_pseudo_legal_moves works properly", "[knight psuedo legal]") {
+  Board board;
+  MoveGenerator move_gen;
+
+  SECTION("single white knight with central pawns") {
+    board.set_piece_positions(BoardConstants::KNIGHT, BoardConstants::WHITE, 0x200000);
+    board.set_piece_positions(BoardConstants::PAWN, BoardConstants::WHITE, 0x8000000);
+    board.set_piece_positions(BoardConstants::PAWN, BoardConstants::BLACK, 0x1000000000);
+
+    std::vector<Move> white_knight_pseudo_legal_moves = move_gen.generate_knight_pseudo_legal_moves(board, BoardConstants::WHITE);
+
+    Move expected_move_one(0x200000, 0x4000000000, BoardConstants::KNIGHT, BoardConstants::NONE, false);
+    Move expected_move_two(0x200000, 0x1000000000, BoardConstants::KNIGHT, BoardConstants::PAWN, false);
+    Move expected_move_three(0x200000, 0x80000000, BoardConstants::KNIGHT, BoardConstants::NONE, false);
+    Move expected_move_four(0x200000, 0x800, BoardConstants::KNIGHT, BoardConstants::NONE, false);
+    Move expected_move_five(0x200000, 0x40, BoardConstants::KNIGHT, BoardConstants::NONE, false);
+    Move expected_move_six(0x200000, 0x10, BoardConstants::KNIGHT, BoardConstants::NONE, false);
+
+    REQUIRE(white_knight_pseudo_legal_moves.size() == 6);
+    REQUIRE(move_vector_contains(expected_move_one, white_knight_pseudo_legal_moves) == true);
+    REQUIRE(move_vector_contains(expected_move_two, white_knight_pseudo_legal_moves) == true);
+    REQUIRE(move_vector_contains(expected_move_three, white_knight_pseudo_legal_moves) == true);
+    REQUIRE(move_vector_contains(expected_move_four, white_knight_pseudo_legal_moves) == true);
+    REQUIRE(move_vector_contains(expected_move_five, white_knight_pseudo_legal_moves) == true);
+    REQUIRE(move_vector_contains(expected_move_six, white_knight_pseudo_legal_moves) == true);
+  }
+}
+*/
